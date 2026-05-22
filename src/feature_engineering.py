654aabf658f7e1f3170c2a0256356df16b20da39
sirius-siru <haudu12343@gmail.com>
2026-05-23 01:24:28 +0700

    add sma, macd, rsi,bollinger bands, atr

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -15,13 +15,60 @@ def calculate_ema(close_price, period):
 
     return pd.Series(ema)
 
-def create_ema_list(close_price, periods_list):
-    result = []
-    for i in periods_list:
-        ema_series = calculate_ema(close_price, i)
-        ema_series.name = f'ema_{i}'
-        ema_series.index = close_price.index
-        result.append(ema_series)
+def calculate_sma(close_price, period):
+    n = close_price.shape[0]
 
-    ema_df = pd.concat(result, axis=1)
+    sma = close_price.rolling(window=period).mean()
+    sma = pd.Series(sma).fillna(0)
+
+    return sma
+
+def calculate_macd(ema12, ema26):
+    return ema12 - ema26
+
+def calculate_rsi(close_price, period):
+    n = close_price.shape[0]
+
+    delta = close_price.diff()
+
+    gain = np.where(delta > 0, delta, 0)
+    loss = np.where(delta < 0, -delta, 0)
+
+    avg_gain = pd.Series(gain).rolling(window=period).mean()
+    avg_loss = pd.Series(loss).rolling(window=period).mean()
+
+    rs = avg_gain / avg_loss
+    rsi = 100 - (100 / (1 + rs))
+
+    return pd.Series(rsi).fillna(0)
+        
+def calculate_bollinger_bands(close_price, middle_band, period):
+    rolling_std = close_price.rolling(window=period).std()
+
+    upper_band = middle_band + (2 * rolling_std)
+    lower_band = middle_band - (2 * rolling_std)
+
+    return upper_band, lower_band
+
+
+def calculate_atr(df, period):
+    n = df.shape[0]
+    tr = np.empty(n)
+    tr[0] = df['high'].iloc[0] - df['low'].iloc[0]
+
+    for i in range(1, n):
+        tr1 = df['high'].iloc[i] - df['low'].iloc[i]
+        tr2 = abs(df['high'].iloc[i] - df['close'].iloc[i-1])
+        tr3 = abs(df['low'].iloc[i] - df['close'].iloc[i-1])
+
+        tr = max([tr1, tr2, tr3])
+
+    atr = pd.Series(tr).rolling(window=period).mean()
+    return atr
+
+
+def calculate_indices(df, periods_list):
+    result = []
+
     return ema_df
+
